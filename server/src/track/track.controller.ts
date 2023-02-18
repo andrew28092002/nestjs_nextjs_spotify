f31395e74bab23bf15a1f3d001de1a1323b2f21d
andrew28092002 <andreiastanaev2002@gmail.com>
2023-02-18 17:14:32 +0500

    add commentTrack func

--- server/src/track/track.controller.ts
+++ server/src/track/track.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { ObjectId } from 'mongoose';
+import { AddCommentDto } from './dto/add-comment.dto';
 import { CreateTrackDto } from './dto/create-track.dto';
 import { TrackService } from './track.service';
 
@@ -25,5 +26,10 @@ export class TrackController {
   @Delete(':id')
   delete(@Param('id') id: ObjectId){
     return this.TrackService.delete(id)
+  }
+
+  @Post('comment')
+  addComment(@Body() dto: AddCommentDto){
+    return this.TrackService.addComment(dto)
   }
 }
--- server/src/track/track.service.ts
+++ server/src/track/track.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, ObjectId } from 'mongoose';
+import { AddCommentDto } from './dto/add-comment.dto';
 import { CreateTrackDto } from './dto/create-track.dto';
 import { Comment, CommentDocument } from './schemas/comment.schema';
 import { Track, TrackDocument } from './schemas/track.schema';
@@ -9,30 +10,41 @@ import { Track, TrackDocument } from './schemas/track.schema';
 export class TrackService {
   constructor(
     @InjectModel(Track.name) private readonly TrackModel: Model<TrackDocument>,
-    @InjectModel(Comment.name) private readonly CommentModel: Model<CommentDocument>
+    @InjectModel(Comment.name)
+    private readonly CommentModel: Model<CommentDocument>,
   ) {}
 
-  async create(dto: CreateTrackDto): Promise<Track>{
-    const track = await this.TrackModel.create(dto)
+  async create(dto: CreateTrackDto): Promise<Track> {
+    const track = await this.TrackModel.create(dto);
 
-    return track
+    return track;
   }
 
-  async getAll(): Promise<Track[]>{
-    const tracks =  await this.TrackModel.find()
+  async getAll(): Promise<Track[]> {
+    const tracks = await this.TrackModel.find();
 
-    return tracks
+    return tracks;
   }
 
-  async getOne(id: ObjectId): Promise<Track>{
-    const track =  await this.TrackModel.findById(id)
+  async getOne(id: ObjectId): Promise<Track> {
+    const track = await this.TrackModel.findById(id);
 
-    return track
+    return track;
   }
 
-  async delete(id: ObjectId): Promise<ObjectId>{
-    await this.TrackModel.findByIdAndDelete(id)
+  async delete(id: ObjectId): Promise<ObjectId> {
+    await this.TrackModel.findByIdAndDelete(id);
 
-    return id
+    return id;
+  }
+
+  async addComment(dto: AddCommentDto): Promise<Comment> {
+    const track = await this.TrackModel.findById(dto.trackId);
+    const comment = await this.CommentModel.create(dto);
+    
+    track.comments.push(comment.id);
+    await track.save();
+
+    return comment;
   }
 }
