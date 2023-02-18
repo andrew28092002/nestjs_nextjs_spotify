2eea3b682371456eb9c654afd6b1ad4bb875e892
andrew28092002 <andreiastanaev2002@gmail.com>
2023-02-18 18:15:00 +0500

    add fileService createFile

--- server/src/file/file.service.ts
+++ server/src/file/file.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import * as path from 'path';
+import * as fs from 'fs';
+import * as uuid from 'uuid';
+
+export enum FileType {
+  AUDIO = 'audio',
+  IMAGE = 'image',
+}
 
 @Injectable()
-export class FileService {}
+export class FileService {
+  createFile(type: FileType, file): string {
+    try {
+      const fileExtension = file.originalname.split('.').pop();
+      const fileName = uuid.v4() + fileExtension;
+      const filePath = path.resolve(__dirname, '..', 'static');
+
+      if (!fs.existsSync(filePath)) {
+        fs.mkdirSync(filePath, { recursive: true });
+      }
+
+      fs.writeFileSync(path.resolve(filePath, fileName), file.buffer);
+
+      return String(type + '/' + fileName);
+    } catch (e) {
+      throw new HttpException(e.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+}
