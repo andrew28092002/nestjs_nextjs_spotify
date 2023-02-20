de80ab64240c4d22192f90ad13fba84dfc2deba8
andrew28092002 <andreiastanaev2002@gmail.com>
2023-02-20 11:16:27 +0500

    update track items structure

--- client/src/components/TrackItem.tsx
+++ client/src/components/TrackItem.tsx
@@ -1,6 +1,9 @@
 import { ITrack } from "@/types/track";
-import { Card } from "@mui/material";
+import { Delete, MoreHoriz, Pause, PlayArrow } from "@mui/icons-material";
+import { Card, Grid, IconButton } from "@mui/material";
+import { useRouter } from "next/router";
 import React, { FC } from "react";
+import styles from "./../styles/TrackItem.module.scss";
 
 type Props = {
   track: ITrack;
@@ -8,7 +11,31 @@ type Props = {
 };
 
 const TrackItem: FC<Props> = ({ track, active = false }) => {
-  return <Card>{track.name}</Card>;
+  const router = useRouter();
+  return (
+    <Card className={styles.track}>
+      <IconButton>{active ? <Pause /> : <PlayArrow />}</IconButton>
+      <img src={track.picture} />
+      <Grid
+        container
+        direction="column"
+        style={{ width: "200px", margin: "0 20px" }}
+      >
+        <div>{track.name}</div>
+        <div style={{ fontSize: "12px", color: "gray" }}>{track.artist}</div>
+      </Grid>
+      {active && <div>0:22 / 3:22</div>}
+      <IconButton style={{ marginLeft: "auto" }}>
+        <Delete />
+      </IconButton>
+      <IconButton
+        style={{ marginLeft: "auto" }}
+        onClick={() => router.push("/track/" + track._id)}
+       >
+        <MoreHoriz />
+      </IconButton>
+    </Card>
+  );
 };
 
 export default TrackItem;
---
+++ client/src/pages/tracks/[id].tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TrackPage = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default TrackPage
