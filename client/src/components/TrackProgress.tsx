9967e318251c2ffabbab05ca02d6457c7ed59e7b
andrew28092002 <andreiastanaev2002@gmail.com>
2023-02-21 11:28:21 +0500

    track_progress-component: add properties at inputand change type for onChangeFunction

--- client/src/components/TrackProgress.tsx
+++ client/src/components/TrackProgress.tsx
@@ -3,13 +3,13 @@ import React, { FC } from "react";
 type Props = {
   left: number;
   right: number;
-  onChange: Function;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
 };
 
-const TrackProgress: FC<Props> = ({ left, right }) => {
+const TrackProgress: FC<Props> = ({ left, right, onChange }) => {
   return (
     <div style={{ display: "flex" }}>
-      <input type="range" />
+      <input type="range" min={left} value={left} max={right} onChange={onChange} />
       <div>
         {left} / {right}
       </div>
