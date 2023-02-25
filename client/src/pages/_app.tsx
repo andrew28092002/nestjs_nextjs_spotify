7539875a8170a0233285f1861b0b55a5e0cdcca3
andrew28092002 <andreiastanaev2002@gmail.com>
2023-02-25 15:10:36 +0500

    create apollo client and provided

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,11 +1,24 @@
 import { store } from "@/store/store";
 import { AppProps } from "next/app";
 import { Provider } from "react-redux";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
+});
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
-  )
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </ApolloProvider>
+  );
 }
