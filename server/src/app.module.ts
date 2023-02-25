c44f63a5973b26d7375d62ba0cd4b3df4338ad91
andrew28092002 <andreiastanaev2002@gmail.com>
2023-02-25 12:39:43 +0500

    added graphql-module in app-module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,10 +6,19 @@ import { AppService } from './app.service';
 import { TrackModule } from './track/track.module';
 import { FileModule } from './file/file.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { resolve } from 'path';
+import { join, resolve } from 'path';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
   imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      debug: false,
+      playground: true,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true
+    }),
     ServeStaticModule.forRoot({
       rootPath: resolve(__dirname, 'static'),
     }),
