9b371624bd335416b92c5dfe029f1b7024375b78
andrew28092002 <andreiastanaev2002@gmail.com>
2023-02-25 13:18:23 +0500

    update create-track-dto

--- server/src/track/dto/create-track.dto.ts
+++ server/src/track/dto/create-track.dto.ts
@@ -1,5 +1,14 @@
+import { ArgsType, Field } from "@nestjs/graphql"
+
+
+@ArgsType()
 export class CreateTrackDto{
+    @Field(type => String, {nullable: false})
     readonly name: string
+
+    @Field(type => String, {nullable: false})
     readonly artist: string
+
+    @Field(type => String, {nullable: false})
     readonly text: string
 }
