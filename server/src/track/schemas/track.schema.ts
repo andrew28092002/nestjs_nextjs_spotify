cadb04f014f0b973646c282f7403414a27e9fa32
andrew28092002 <andreiastanaev2002@gmail.com>
2023-02-25 12:39:54 +0500

    update track-schema

--- server/src/track/schemas/track.schema.ts
+++ server/src/track/schemas/track.schema.ts
@@ -1,29 +1,42 @@
+import { Field, ID, ObjectType } from '@nestjs/graphql';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument, Types } from 'mongoose';
+import { Comment } from './comment.schema';
 
 export type TrackDocument = HydratedDocument<Track>;
 
+@ObjectType()
 @Schema()
 export class Track {
+  @Field(type => ID)
+  _id: string
+
   @Prop({required: true})
+  @Field()
   name: string;
 
   @Prop({required: true})
+  @Field()
   artist: string;
 
   @Prop({required: true})
+  @Field()
   text: string;
 
   @Prop({default: 0})
+  @Field()
   listens: number;
 
   @Prop()
+  @Field()
   picture: string;
 
   @Prop()
+  @Field()
   audio: string;
 
   @Prop({ type: [{ type: Types.ObjectId, ref: 'Comment' }] })
+  @Field(type => [Comment])
   comments: Comment[];
 }
 
